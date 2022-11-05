40f11c7d34bf6101a713281497643eb929e4cb21
honeySimulator <ovsysha@gamil.com>
2022-11-05 15:32:24 +0300

    #DEMO-36

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,12 @@ var scroll = new SmoothScroll('a[href*="#"]', {
     speed: 500,
     speedAsDuration: true
 });
+
+//Sticky Menu
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 50) {
+        document.getElementById("main-nav").className = "scrolling";
+    } else {
+        document.getElementById("main-nav").className = "";
+    }
+})
